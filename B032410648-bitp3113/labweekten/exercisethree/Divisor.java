9e6625025087681cab12777a593feecf42db4713
Apitpr0 <apitpro123@gmail.com>
2025-05-27 08:49:21 +0800

    labweek10 - new file

---
+++ B032410648-bitp3113/labweekten/exercisethree/Divisor.java
@@ -0,0 +1,23 @@
+package labexception.exetwo;
+
+/**
+ * BITP 3113 Object Oriented Programming
+ * 
+ * This class represents a division operator
+ * 
+ * @author Emaliana Kasmuri, FTMK, UTeM
+ */
+public class Divisor {
+	
+	/**
+	 * This method perform division of two number
+	 * @param divident
+	 * @param divisor
+	 * @return division result
+	 */
+	public int performDivision (int divident, int divisor) {
+		
+		return divident/divisor;
+	}
+
+}
